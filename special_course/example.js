eba3da0ef6e18756ede1c48422b2229b264c48c9
alamin <alamindawancmt@gmail.com>
2026-07-20 22:53:45 +0600

    generate ts to js by tsc command

---
+++ special_course/example.js
@@ -0,0 +1,10 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+console.log(add(19, 6));
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(10, 20));
+console.log("hello world");
---
+++ special_course/example.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number): number{
+    return a + b;
+}
+
+console.log(add(19,6))
+
+function multiply(a: number, b: number): number{
+    return a * b;
+}
+
+console.log(multiply(10,20))
+console.log("hello world")
