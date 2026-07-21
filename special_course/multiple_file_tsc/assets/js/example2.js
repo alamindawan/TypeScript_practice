60b76d5101eef88334e4f69a2739b5d0008d17cc
alamin <alamindawancmt@gmail.com>
2026-07-21 16:18:57 +0600

    multiple file TSC with directory

---
+++ special_course/multiple_file_tsc/assets/js/example2.js
@@ -0,0 +1,10 @@
+"use strict";
+function add2(a, b) {
+    return a + b;
+}
+console.log(add2(19, 6));
+function multiply2(a, b) {
+    return a * b;
+}
+console.log(multiply2(10, 20));
+console.log("hello world");
---
+++ special_course/multiple_file_tsc/assets/ts/example2.ts
@@ -0,0 +1,12 @@
+function add2(a: number, b: number): number{
+    return a + b;
+}
+
+console.log(add2(19,6))
+
+function multiply2(a: number, b: number): number{
+    return a * b;
+}
+
+console.log(multiply2(10,20))
+console.log("hello world")
